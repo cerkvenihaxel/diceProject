fbdea0d223fd4d4faf8db41c3395ed9d5ad220a2
Axel Cerkvenih <cerkvenihaxel@gmail.com>
2023-06-06 11:56:59 -0300

    Dice project

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceImages = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+      
+        
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let randomIndex1
+        =  Int(arc4random_uniform(UInt32(diceImages.count)))
+        
+        let randomIndex2
+        = Int(arc4random_uniform(UInt32(diceImages.count)))
+
+                diceImageView1.image = UIImage(named: diceImages[randomIndex1])
+                diceImageView2.image = UIImage(named: diceImages[randomIndex2])
+
+                print("""
+                
+                Dice 1 : NUMBER \(randomIndex1 + 1)
+                Dice 2 : NUMBER \(randomIndex2 + 1)
+                
+                """)
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,21 @@
+![axelcrkv Banner](Documentation/banner-axel.png)
+
+# Dicee (Ejercicio del iOS Bootcamp)
+
+## Meta
+
+Introducción al core programming de Swift, creando una pequeña y simple app de Dice Roll
+
+
+## Aprendizajes
+
+* Clonar un archivo de Github y subirlo a mi repositorio personal
+* Crear una aplicación con controllers y funcionalidades
+* Crear un enlace entre los archivos del Interface Builder y codificarlos mediante IBActions y IBOutlets.
+* Variables y constantes de Swift.
+* Arrays de Swift
+* Testeo y debugging en XCode
+* Randomización
+
+
+
